e0d64015428fa7e4c341c6958a32571afd061558
Oopdawg <94260533+Oopdawg@users.noreply.github.com>
2025-02-14 20:04:59 -0500

    whoops forgot to add to git

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystemcommand/lift/HangCommand.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.subsystemcommand.lift;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
+
+import org.firstinspires.ftc.teamcode.common.hardware.Robot;
+import org.firstinspires.ftc.teamcode.common.subsystems.LiftSubsystem;
+import static org.firstinspires.ftc.teamcode.common.utils.Globals.*;
+
+public class HangCommand extends SequentialCommandGroup {
+    public HangCommand(Robot robot) {
+        super(
+                new InstantCommand(() -> robot.lift.updateState(LiftSubsystem.LiftState.LVL2_ASCENT_DOWN)),
+                new WaitUntilCommand(()-> robot.liftActuator.getPosition() < ENDGAME_ASCENT_HEIGHT+200)
+        );
+    }
+}
